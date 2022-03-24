5c2e274252f9e37e75bde5b1c4c8415dd079baad
johnjihwan <s20062@gsm.hs.kr>
2022-03-25 02:25:41 +0900

    Add AnonymousEntity draft -1

---
+++ src/main/java/site/hirecruit/hr/domain/anonymous/entity/AnonymousEntity.java
@@ -0,0 +1,40 @@
+package site.hirecruit.hr.domain.anonymous.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+/**
+ * @version 1.0.0
+ * @author 전지환
+ */
+@Table
+@Entity(name = "anonymous")
+@Getter @Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class AnonymousEntity {
+
+    @Id
+    @Column(name = "anonymous_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long anonymousId;
+
+    @NotNull
+    @Column(name = "anonymous_uuid")
+    private String anonymousUUID;
+
+    @NotNull
+    @Column(name = "name")
+    private String name;
+
+    @NotNull
+    @Column(name = "email")
+    private String email;
+
+    @NotNull
+    @Column(name = "email_certified")
+    private boolean email_certified;
+
+}
