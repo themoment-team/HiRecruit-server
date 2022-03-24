f41450537daef0f525517bc305ab88980604c6c5
siwony_ <siwon103305@gmail.com>
2022-03-25 01:48:58 +0900

    init

---
+++ src/main/java/site/hirecruit/hr/HRApplication.java
@@ -0,0 +1,13 @@
+package site.hirecruit.hr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HRApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HRApplication.class, args);
+	}
+
+}
