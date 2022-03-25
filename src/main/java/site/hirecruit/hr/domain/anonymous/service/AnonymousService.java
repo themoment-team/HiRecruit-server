c2d636970ffafe86d96f2599a2f76601c8720e71
johnjihwan <s20062@gsm.hs.kr>
2022-03-25 16:17:20 +0900

    Add 익명 domain service

--- src/main/java/site/hirecruit/hr/domain/anonymous/service/AnonymousService.java
+++ src/main/java/site/hirecruit/hr/domain/anonymous/service/AnonymousService.java
@@ -1,2 +1,25 @@
-package site.hirecruit.hr.domain.anonymous.service;public class AnonymousService {
+package site.hirecruit.hr.domain.anonymous.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import site.hirecruit.hr.domain.anonymous.dto.AnonymousDto;
+import site.hirecruit.hr.domain.anonymous.repository.AnonymousRepository;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class AnonymousService {
+
+    private final AnonymousRepository anonymousRepository;
+
+    /**
+     * anonymousUUID로 익명 정보 조회하는 비즈니스 로직
+     *
+     * @param anonymousUUID 찾고자 하는 익명 UUID
+     * @return AnonymousResponseDto - 조회된 익명 정보
+     */
+    public AnonymousDto.AnonymousResponseDto findAnonymousByUUID(String anonymousUUID){
+        return anonymousRepository.findByAnonymousUUID(anonymousUUID);
+    }
 }
