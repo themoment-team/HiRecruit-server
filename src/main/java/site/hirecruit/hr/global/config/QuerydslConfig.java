340b5d1ab26cd35512abece30770204c20a6a771
johnjihwan <s20062@gsm.hs.kr>
2022-03-25 16:16:26 +0900

    Add global querydsl config

--- src/main/java/site/hirecruit/hr/global/config/QuerydslConfig.java
+++ src/main/java/site/hirecruit/hr/global/config/QuerydslConfig.java
@@ -1,2 +1,19 @@
-package site.hirecruit.hr.global.config;public class QuerydslConfig {
+package site.hirecruit.hr.global.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Configuration
+public class QuerydslConfig {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory(){
+        return new JPAQueryFactory(entityManager);
+    }
 }
