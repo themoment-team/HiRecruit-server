16d0c82bd40531b39cb0c439623300e99112352e
siwony_ <siwon103305@gmail.com>
2022-03-25 15:34:18 +0900

    Rename MentorRepository -> src/main/java/site/hirecruit/hr/domain/worker/repository/WorkerRepository.java

--- src/main/java/site/hirecruit/hr/domain/worker/repository/WorkerRepository.java
+++ src/main/java/site/hirecruit/hr/domain/worker/repository/WorkerRepository.java
@@ -3,5 +3,5 @@ package site.hirecruit.hr.domain.worker.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import site.hirecruit.hr.domain.worker.entity.WorkerEntity;
 
-public interface MentorRepository extends JpaRepository<WorkerEntity, Long> {
+public interface WorkerRepository extends JpaRepository<WorkerEntity, Long> {
 }
