f7304a58884148ac1a65c9468c111f3816d9ae8a
johnjihwan <s20062@gsm.hs.kr>
2022-03-25 16:17:39 +0900

    Add custom query

--- src/main/java/site/hirecruit/hr/domain/anonymous/controller/AnonymousController.java
+++ src/main/java/site/hirecruit/hr/domain/anonymous/controller/AnonymousController.java
@@ -1,13 +1,20 @@
 package site.hirecruit.hr.domain.anonymous.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import site.hirecruit.hr.domain.anonymous.dto.AnonymousDto;
+import site.hirecruit.hr.domain.anonymous.service.AnonymousService;
 
 @RestController
 @RequestMapping("/api/v1/anonymous/")
+@RequiredArgsConstructor
 public class AnonymousController {
+
+    private final AnonymousService anonymousService;
 
     /*
     TODO
@@ -15,8 +22,12 @@ public class AnonymousController {
      */
 
     @GetMapping("")
-    public void findAnonymousByUUID(@RequestParam(value = "id") String anonymousId){
+    public ResponseEntity<AnonymousDto.AnonymousResponseDto> findAnonymousByUUID(@RequestParam(value = "uuid") String anonymousUUID){
+        final AnonymousDto.AnonymousResponseDto anonymousByUUID = anonymousService.findAnonymousByUUID(anonymousUUID);
 
+        //TODO hateoas
+
+        return ResponseEntity.ok().body(anonymousByUUID);
     }
 
 }
