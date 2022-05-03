9909abbe7ea09a229be0952c2c9f26b681f0a9ab
siwony_ <siwon103305@gmail.com>
2022-05-03 17:19:05 +0900

    Update logging을  kotlin-logging-jvm라이브러리로 변경

--- src/main/java/site/hirecruit/hr/global/config/EmbeddedRedisConfig.kt
+++ src/main/java/site/hirecruit/hr/global/config/EmbeddedRedisConfig.kt
@@ -1,12 +1,14 @@
 package site.hirecruit.hr.global.config
 
-import org.slf4j.LoggerFactory
+import mu.KotlinLogging
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
 import redis.embedded.RedisServer
 import javax.annotation.PostConstruct
 import javax.annotation.PreDestroy
+
+private val log = KotlinLogging.logger {}
 
 /**
  * Embedded redis config
@@ -20,9 +22,6 @@ import javax.annotation.PreDestroy
 class EmbeddedRedisConfig(
     @Value("\${spring.redis.port}") val redisPort: Int
 ) {
-
-    private val log = LoggerFactory.getLogger(this.javaClass)
-
     init {
         log.info("embedded redis will start port = '{}'", redisPort)
     }
