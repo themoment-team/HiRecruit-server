beb7cbe33d1b9ba825b0004b745039d6100217db
siwony_ <siwon103305@gmail.com>
2022-05-06 04:14:51 +0900

    Update @jyeonjyan 님이 제안한 @Repository 달아주기

--- src/main/java/site/hirecruit/hr/domain/worker/repository/WorkerRepository.kt
+++ src/main/java/site/hirecruit/hr/domain/worker/repository/WorkerRepository.kt
@@ -1,8 +1,10 @@
 package site.hirecruit.hr.domain.worker.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 import site.hirecruit.hr.domain.worker.entity.WorkerEntity
 
+@Repository
 interface WorkerRepository : JpaRepository<WorkerEntity, Long> {
 
     fun existsByGithubId(id: Long): Boolean
