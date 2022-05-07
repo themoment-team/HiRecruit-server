18839abdefadfccb0a1a47e76d7589c37837d1ca
siwony_ <siwon103305@gmail.com>
2022-05-07 19:54:31 +0900

    Add AuthProcessorImpl

---
+++ src/main/java/site/hirecruit/hr/domain/auth/service/AuthProcessorImpl.kt
@@ -0,0 +1,24 @@
+package site.hirecruit.hr.domain.auth.service
+
+import org.springframework.stereotype.Service
+import site.hirecruit.hr.domain.auth.dto.OAuthAttributes
+import site.hirecruit.hr.domain.auth.model.User
+import site.hirecruit.hr.domain.worker.repository.WorkerRepository
+
+@Service
+class AuthProcessorImpl(
+    private val workerRepository: WorkerRepository,
+    private val userRegistrationService: UserRegistrationService,
+    private val userAuthService: UserAuthService
+) : AuthProcessor {
+
+
+    override fun process(oauthAttributes: OAuthAttributes): User {
+        // 첫 사용자라면 계정을 등록한다.
+        if(workerRepository.existsByGithubId(oauthAttributes.id))
+            userRegistrationService.registration(oauthAttributes)
+
+        return userAuthService.authentication(oauthAttributes)
+    }
+
+}
