3786ba769f2ed1471c4c74b10e6c5dc95dfa2359
johnjihwan <s20062@gsm.hs.kr>
2022-05-10 14:40:15 +0900

    Delete 정적 유틸리티 클래스 없애기

---
+++ src/main/java/site/hirecruit/hr/domain/mentee/dto/MenteeDto.kt
@@ -0,0 +1,4 @@
+package site.hirecruit.hr.domain.mentee.dto
+
+class MenteeDto {
+}
---
+++ src/test/java/site/hirecruit/hr/domain/mentee/dto/MenteeDtoTest.kt
@@ -0,0 +1,5 @@
+package site.hirecruit.hr.domain.mentee.dto
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class MenteeDtoTest
