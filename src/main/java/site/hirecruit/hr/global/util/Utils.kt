7512f64e47bce46528146e76fa57b549ba64ce25
johnjihwan <s20062@gsm.hs.kr>
2022-05-10 14:40:31 +0900

    Add 최상위 함수를 정의할 Utils.kt

--- src/main/java/site/hirecruit/hr/global/util/Utils.kt
+++ src/main/java/site/hirecruit/hr/global/util/Utils.kt
@@ -1,4 +1,18 @@
 package site.hirecruit.hr.global.util
 
-class Utils {
+import java.util.*
+
+fun uuidGenerator(email: String): String {
+    val charset = Charsets.UTF_8
+    val byteArray = email.toByteArray(charset)
+    val uuid = UUID.nameUUIDFromBytes(byteArray)
+
+    fun getUUIDInfoDetails() {
+        println("payloadData = $email")
+        println("charset = $charset")
+        println("byteArray = ${byteArray.contentToString()}")
+        println("uuid = $uuid")
+    }
+
+    return uuid.toString()
 }
