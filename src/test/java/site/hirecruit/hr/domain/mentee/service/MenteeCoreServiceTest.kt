3c976d33d85d438cf6e147f0391183d12dea642b
johnjihwan <s20062@gsm.hs.kr>
2022-05-11 09:04:20 +0900

    Add mentee 조회 비즈니스 로직 테스트

--- src/test/java/site/hirecruit/hr/domain/mentee/service/MenteeCoreServiceTest.kt
+++ src/test/java/site/hirecruit/hr/domain/mentee/service/MenteeCoreServiceTest.kt
@@ -1,6 +1,7 @@
 package site.hirecruit.hr.domain.mentee.service
 
 import net.bytebuddy.utility.RandomString
+import org.assertj.core.api.Assertions.assertThat
 import org.hibernate.validator.internal.util.Contracts.assertNotNull
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
@@ -16,7 +17,7 @@ class MenteeCoreServiceTest(
     @Autowired private var menteeRepository: MenteeRepository
 ) {
 
-    var anonymous: MenteeEntity? = null
+    var setUpMentee: MenteeEntity? = null
 
     @BeforeEach
     internal fun setUp() {
@@ -28,12 +29,12 @@ class MenteeCoreServiceTest(
             false)
 
 
-        anonymous = menteeRepository.save(menteeEntity)
+        setUpMentee = menteeRepository.save(menteeEntity)
     }
 
     @Test
     @DisplayName("멘티로 등록한다. [valid에는 관심이 없는 테스트]")
-    fun 멘티로_등록한다(
+    fun 멘티를_등록하면_등록된_정보를_정상적으로_반환한다(
         @Autowired menteeService: MenteeService
     ){
 
@@ -50,4 +51,17 @@ class MenteeCoreServiceTest(
         assertNotNull(registerMentee.email)
         assertNotNull(registerMentee.emailCertified)
     }
+
+    @Test
+    fun menteeUUID로_조회하면_해당하는_mentee_정보가_반환된다(
+        @Autowired menteeService: MenteeService
+    ){
+        // when
+        val findMentee = setUpMentee?.menteeUUID?.let { menteeService.findMenteeByUUID(it) }
+
+        // then
+        assertNotNull(findMentee)
+        assertThat(findMentee?.menteeId).isEqualTo(setUpMentee?.menteeId)
+    }
+
 }
