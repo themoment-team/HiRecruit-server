6fd499c6cba9ca51f6bd866d980c6272bc14cc7f
siwony_ <siwon103305@gmail.com>
2022-05-12 15:10:07 +0900

    Fix TempUserEntity @Id import 실수
    Description javax.persistence.Id 대신 org.springframework.data.annotation.Id import 함

--- src/main/java/site/hirecruit/hr/domain/auth/entity/TempUserEntity.kt
+++ src/main/java/site/hirecruit/hr/domain/auth/entity/TempUserEntity.kt
@@ -1,9 +1,8 @@
 package site.hirecruit.hr.domain.auth.entity
 
+import org.springframework.data.annotation.Id
 import org.springframework.data.redis.core.RedisHash
 import javax.persistence.Column
-import javax.persistence.Id
-
 /**
  * 임시 유저를 저장하는 Entity
  * Redis namespace는 temp_user를 사용하고 1시간의 유효기간을 갖습니다.
