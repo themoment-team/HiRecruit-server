57f3b58fc88e29ccfe97f9e6ef251875e4109935
siwony_ <siwon103305@gmail.com>
2022-05-19 17:57:27 +0900

    Add CurrentAuthUserInfoResolver 로직 작성
    Description session에서 AuthUserInfo를 가져오는 HandlerMethodArgumentResolver 만듦

---
+++ src/main/java/site/hirecruit/hr/global/resolver/CurrentAuthUserInfoResolver.kt
@@ -0,0 +1,38 @@
+package site.hirecruit.hr.global.resolver
+
+import org.springframework.core.MethodParameter
+import org.springframework.http.HttpStatus
+import org.springframework.stereotype.Component
+import org.springframework.web.bind.support.WebDataBinderFactory
+import org.springframework.web.client.HttpClientErrorException
+import org.springframework.web.context.request.NativeWebRequest
+import org.springframework.web.method.support.HandlerMethodArgumentResolver
+import org.springframework.web.method.support.ModelAndViewContainer
+import site.hirecruit.hr.domain.auth.annotation.CurrentAuthUserInfo
+import javax.servlet.http.HttpSession
+
+/**
+ * @author 정시원
+ * @since 1.0
+ */
+@Component
+class CurrentAuthUserInfoResolver(
+    private val httpSession: HttpSession
+): HandlerMethodArgumentResolver {
+
+    override fun supportsParameter(parameter: MethodParameter): Boolean {
+        return parameter.hasParameterAnnotation(CurrentAuthUserInfo::class.java);
+    }
+
+    override fun resolveArgument(
+        parameter: MethodParameter,
+        mavContainer: ModelAndViewContainer?,
+        webRequest: NativeWebRequest,
+        binderFactory: WebDataBinderFactory?
+    ): Any? {
+        return httpSession.getAttribute("authUserInfo")
+            ?: throw HttpClientErrorException(HttpStatus.UNAUTHORIZED)
+    }
+
+
+}
