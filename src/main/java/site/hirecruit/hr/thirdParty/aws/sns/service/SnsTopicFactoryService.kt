fa8021fd08cc2c83844d6a439ffebe9b3331e7e8
johnjihwan <s20062@gsm.hs.kr>
2022-05-20 18:16:53 +0900

    Add SnsTopic structure -draft

---
+++ src/main/java/site/hirecruit/hr/thirdParty/aws/sns/service/SnsTopicFactoryService.kt
@@ -0,0 +1,11 @@
+package site.hirecruit.hr.thirdParty.aws.sns.service
+
+import org.springframework.stereotype.Service
+
+@Service
+class SnsTopicFactoryService {
+
+    fun createTopic(topicName: String){
+
+    }
+}
