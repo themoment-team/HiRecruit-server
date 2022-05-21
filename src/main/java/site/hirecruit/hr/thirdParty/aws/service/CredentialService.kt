6af3597ffd050b9fd0afdad86c686de729b7e951
johnjihwan <s20062@gsm.hs.kr>
2022-05-21 18:05:24 +0900

    Update private method:: credential 을 얻는 메소드는 snsClient 를 생성할 떄만 사용함 캡슐화

--- src/main/java/site/hirecruit/hr/thirdParty/aws/service/CredentialService.kt
+++ src/main/java/site/hirecruit/hr/thirdParty/aws/service/CredentialService.kt
@@ -16,17 +16,28 @@ import software.amazon.awssdk.services.sns.SnsClient
 @Service
 class CredentialService(private val awsSnsConfig: AwsSnsConfig) {
 
-    fun getAwsCredentials(accessKey: String, secretKey: String): AwsCredentialsProvider {
-        return AwsCredentialsProvider {
-            AwsBasicCredentials.create(accessKey, secretKey)
-        }
-    }
-
+    /**
+     * aws sns 서비스의 permission이 있는 IAM의 자격을 얻어 properties에 알맞는 region으로 SnsClient를 제공해준다.
+     *
+     * @see AwsSnsConfig sns전용 IAM에 대한 설정
+     */
     fun getSnsClient(): SnsClient {
+
         return SnsClient.builder()
             .credentialsProvider(
                 getAwsCredentials(awsSnsConfig.awsAccessKey, awsSnsConfig.awsSecretKey)
             ).region(Region.of(awsSnsConfig.awsRegion))
             .build()
+    }
+
+    /**
+     * accessKey, secretKey 로 해당 IAM 의 자격을 얻는다.
+     *
+     * @see getSnsClient - credentials가 필요한 부분에 넣어준다.
+     */
+    private fun getAwsCredentials(accessKey: String, secretKey: String): AwsCredentialsProvider {
+        return AwsCredentialsProvider {
+            AwsBasicCredentials.create(accessKey, secretKey)
+        }
     }
 }
