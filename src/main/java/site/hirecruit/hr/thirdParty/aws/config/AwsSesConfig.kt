a5f17b51c4d167f1d1aace97c352874480df9175
johnjihwan <s20062@gsm.hs.kr>
2022-05-26 12:12:17 +0900

    Add aws ses config

--- src/main/java/site/hirecruit/hr/thirdParty/aws/config/AwsSesConfig.kt
+++ src/main/java/site/hirecruit/hr/thirdParty/aws/config/AwsSesConfig.kt
@@ -1,10 +1,39 @@
 package site.hirecruit.hr.thirdParty.aws.config
 
+import mu.KotlinLogging
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.PropertySource
+import javax.annotation.PostConstruct
+
+
+private val log = KotlinLogging.logger {}
+
 /**
  * aws ses service 를 쓰기위한 설정 정보들이 init 됩니다.
  *
  * @author 전지환
  * @since 1.0.0
  */
-class AwsSesConfig {
+@Configuration
+@PropertySource("classpath:/aws/aws-ses-config.properties")
+class AwsSesConfig(
+    @Value("\${aws.accessKey}")
+    val awsAccessKey: String,
+
+    @Value("\${aws.secretKey}")
+    val awsSecretKey: String,
+
+    @Value("\${aws.region}")
+    val awsRegion: String
+){
+
+    @PostConstruct
+    fun init() {
+        log.debug { "AwsSnsConfig(" +
+                "awsAccessKey='$awsAccessKey', " +
+                "awsSecretKey='$awsSecretKey', " +
+                "awsRegion='$awsRegion'" +
+                ")" }
+    }
 }
