dc4d05c92be7698bbca308987a74ca414d3993d7
siwony_ <siwon103305@gmail.com>
2022-05-28 14:04:06 +0900

    Update security config role test url

--- src/main/java/site/hirecruit/hr/global/security/SecurityConfig.kt
+++ src/main/java/site/hirecruit/hr/global/security/SecurityConfig.kt
@@ -3,13 +3,16 @@ package site.hirecruit.hr.global.security
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService
 import org.springframework.security.oauth2.core.user.OAuth2User
+import site.hirecruit.hr.domain.auth.entity.Role
 
 @Configuration
 class SecurityConfig(
-    private val oauth2UserService: OAuth2UserService<OAuth2UserRequest, OAuth2User>
+    private val oauth2UserService: OAuth2UserService<OAuth2UserRequest, OAuth2User>,
+//    private val userDetailsService: UserDetailsService
     ) : WebSecurityConfigurerAdapter() {
 
     override fun configure(http: HttpSecurity) {
@@ -24,6 +27,9 @@ class SecurityConfig(
                 "/js/**", "/h2-console/**"
             ).permitAll() //                .antMatchers("/api/v1/**")
             //                .hasRole(WorkerEntity.Role.CLIENT.name())
+            .antMatchers("/test/email").hasRole(Role.UNAUTHENTICATED_EMAIL.name)
+            .antMatchers("/test/client").hasRole(Role.CLIENT.name)
+            .antMatchers("/test/guest").hasRole(Role.GUEST.name)
             .anyRequest().permitAll()
 
         http
@@ -33,6 +39,6 @@ class SecurityConfig(
         http
             .oauth2Login()
             .userInfoEndpoint()
-            .userService(oauth2UserService)
+            .userService(this.oauth2UserService)
     }
 }
