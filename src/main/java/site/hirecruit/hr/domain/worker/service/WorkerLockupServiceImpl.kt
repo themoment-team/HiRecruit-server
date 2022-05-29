4fc5be3351b01745c01da8c5d4f1a768d3560d73
siwony_ <siwon103305@gmail.com>
2022-05-30 02:05:34 +0900

    Add WOrkerLockupServiceImpl

---
+++ src/main/java/site/hirecruit/hr/domain/worker/service/WorkerLockupServiceImpl.kt
@@ -0,0 +1,27 @@
+package site.hirecruit.hr.domain.worker.service
+
+import org.springframework.stereotype.Service
+import site.hirecruit.hr.domain.worker.dto.WorkerDto
+import site.hirecruit.hr.domain.worker.repository.WorkerRepository
+
+/**
+ * WorkerLockupService 구현체
+ *
+ * @author 정시원
+ * @since 1.0
+ */
+@Service
+class WorkerLockupServiceImpl(
+    private val workerRepository: WorkerRepository
+): WorkerLockupService {
+
+
+    override fun findByWorkerId(workerId: Long): WorkerDto.Info =
+        workerRepository.findWorkerInfoDtoByWorkerId(workerId)
+
+    override fun findByCompanyId(companyId: Long): WorkerDto.Info =
+        workerRepository.findWorkerInfoDtoByCompanyId(companyId)
+
+    override fun findAll(): WorkerDto.Info =
+        workerRepository.findWorkerInfoDtoByAll()
+}
