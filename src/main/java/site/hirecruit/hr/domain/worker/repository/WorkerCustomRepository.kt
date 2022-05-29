5a7e5dcc1e5e99479c9cb3aaec918cb5063f7a85
siwony_ <siwon103305@gmail.com>
2022-05-30 02:03:49 +0900

    Add WorkerCustomRepository interface

---
+++ src/main/java/site/hirecruit/hr/domain/worker/repository/WorkerCustomRepository.kt
@@ -0,0 +1,18 @@
+package site.hirecruit.hr.domain.worker.repository
+
+import site.hirecruit.hr.domain.worker.dto.WorkerDto
+
+/**
+ * WorkerCustomRepository
+ *
+ * @author 정시원
+ * @since 1.0
+ */
+interface WorkerCustomRepository {
+
+    fun findWorkerInfoDtoByWorkerId(workerId: Long): WorkerDto.Info
+
+    fun findWorkerInfoDtoByCompanyId(companyId: Long): WorkerDto.Info
+
+    fun findWorkerInfoDtoByAll(): WorkerDto.Info
+}
--- src/main/java/site/hirecruit/hr/domain/worker/repository/WorkerRepository.kt
+++ src/main/java/site/hirecruit/hr/domain/worker/repository/WorkerRepository.kt
@@ -2,10 +2,11 @@ package site.hirecruit.hr.domain.worker.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import site.hirecruit.hr.domain.worker.dto.WorkerDto
 import site.hirecruit.hr.domain.worker.entity.WorkerEntity
 
 @Repository
-interface WorkerRepository : JpaRepository<WorkerEntity, Long> {
+interface WorkerRepository : JpaRepository<WorkerEntity, Long>, WorkerCustomRepository {
 
     fun findByUser_GithubId(githubId: Long): WorkerEntity?
 }
