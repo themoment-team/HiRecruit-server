272df405bcb28bf89a0a4b98638d0c69a6c61a20
siwony_ <siwon103305@gmail.com>
2022-05-29 19:44:58 +0900

    Update WorkerRegistrationServiceImpl company 및 position 저장 추가

--- src/main/java/site/hirecruit/hr/domain/worker/service/WorkerRegistrationServiceImpl.kt
+++ src/main/java/site/hirecruit/hr/domain/worker/service/WorkerRegistrationServiceImpl.kt
@@ -1,8 +1,13 @@
 package site.hirecruit.hr.domain.worker.service
 
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import site.hirecruit.hr.domain.auth.dto.AuthUserInfo
 import site.hirecruit.hr.domain.auth.repository.UserRepository
+import site.hirecruit.hr.domain.company.dto.CompanyDto
+import site.hirecruit.hr.domain.company.entity.CompanyEntity
+import site.hirecruit.hr.domain.company.repository.CompanyRepository
+import site.hirecruit.hr.domain.company.service.CompanyService
 import site.hirecruit.hr.domain.worker.dto.WorkerDto
 import site.hirecruit.hr.domain.worker.entity.WorkerEntity
 import site.hirecruit.hr.domain.worker.repository.WorkerRepository
@@ -16,29 +21,39 @@ import site.hirecruit.hr.domain.worker.repository.WorkerRepository
 @Service
 class WorkerRegistrationServiceImpl(
     private val userRepository: UserRepository,
-    private val workerRepository: WorkerRepository
+    private val workerRepository: WorkerRepository,
+    private val companyRepository: CompanyRepository
 ): WorkerRegistrationService {
 
     override fun registration(authUserInfo: AuthUserInfo, registrationDto: WorkerDto.Registration): WorkerDto.Info{
         val userEntity = userRepository.findByGithubId(authUserInfo.githubId)
             ?: throw IllegalStateException("Cannot found UserEntity, githubId='${authUserInfo.githubId}'")
+        val companyEntity = companyRepository.findByIdOrNull(registrationDto.companyId)
+            ?: throw IllegalArgumentException("Cannot found CompanyEntity, companyId='${registrationDto.companyId}'")
+
         val savedWorkerEntity = workerRepository.save(
             WorkerEntity(
                 introduction = registrationDto.introduction,
                 giveLink = registrationDto.giveLink,
                 devYear = registrationDto.devYear,
-                user = userEntity
+                position = registrationDto.position,
+                user = userEntity,
+                company = companyEntity
             )
         )
         return WorkerDto.Info(
             name = authUserInfo.name,
             email = authUserInfo.email!!,
             profileImgUri = authUserInfo.profileImgUri,
-            companyName = savedWorkerEntity.companyName,
-            location = savedWorkerEntity.location,
-            introduction = savedWorkerEntity.introduction,
             giveLink = savedWorkerEntity.giveLink,
-            devYear = savedWorkerEntity.devYear
+            devYear = savedWorkerEntity.devYear,
+            position = savedWorkerEntity.position,
+            companyInfoDto = CompanyDto.Info(
+                companyId = companyEntity.companyId!!,
+                name = companyEntity.name,
+                location = companyEntity.location,
+                imageUri = companyEntity.imageUri
+            )
         )
     }
 
