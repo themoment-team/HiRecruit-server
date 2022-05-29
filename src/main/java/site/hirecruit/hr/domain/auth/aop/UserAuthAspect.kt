ecedd615b9e34715816d03630d251d7bef69904f
siwony_ <siwon103305@gmail.com>
2022-05-29 17:33:33 +0900

    Update logic rollback

--- src/main/java/site/hirecruit/hr/domain/auth/aop/UserAuthAspect.kt
+++ src/main/java/site/hirecruit/hr/domain/auth/aop/UserAuthAspect.kt
@@ -4,6 +4,7 @@ import mu.KotlinLogging
 import org.aspectj.lang.annotation.AfterReturning
 import org.aspectj.lang.annotation.Aspect
 import org.aspectj.lang.annotation.Pointcut
+import org.springframework.security.oauth2.client.web.HttpSessionOAuth2AuthorizedClientRepository
 import org.springframework.stereotype.Component
 import site.hirecruit.hr.domain.auth.dto.AuthUserInfo
 import site.hirecruit.hr.global.data.SessionAttribute
@@ -35,10 +36,8 @@ class UserAuthAspect (
     )
     private fun setSessionByAuthUserInfo(authUserInfo: AuthUserInfo): Any{
         log.debug("UserAuthAspect.setSessionByAuthUserInfo active")
-        if(httpSession.isNew) { // 이미 세션이 있는 경우 set하지 않는다.
-            httpSession.setAttribute(SessionAttribute.AUTH_USER_INFO.attributeName, authUserInfo)
-            log.debug("session already active")
-        }
+        httpSession.setAttribute(SessionAttribute.AUTH_USER_INFO.attributeName, authUserInfo)
+        log.debug("session already active")
         log.debug("session id='${httpSession.id}', authUserInfo='${httpSession.getAttribute(SessionAttribute.AUTH_USER_INFO.attributeName)}'")
         return authUserInfo
     }
