5a69f4da4255e5570a72aaefb76087ddd6e77739
siwony_ <siwon103305@gmail.com>
2022-05-30 02:32:05 +0900

    Update 조회 API'ㅎ

--- src/main/java/site/hirecruit/hr/domain/worker/controller/WorkerController.kt
+++ src/main/java/site/hirecruit/hr/domain/worker/controller/WorkerController.kt
@@ -4,12 +4,15 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PatchMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import site.hirecruit.hr.domain.auth.dto.AuthUserInfo
 import site.hirecruit.hr.domain.worker.dto.WorkerDto
 import site.hirecruit.hr.domain.worker.service.AuthUserWorkerService
+import site.hirecruit.hr.domain.worker.service.WorkerLockupService
 import site.hirecruit.hr.global.annotation.CurrentAuthUserInfo
 import springfox.documentation.annotations.ApiIgnore
 
@@ -22,7 +25,8 @@ import springfox.documentation.annotations.ApiIgnore
 @RestController
 @RequestMapping("/api/v1/worker")
 class WorkerController(
-    private val authUserWorkerService: AuthUserWorkerService
+    private val authUserWorkerService: AuthUserWorkerService,
+    private val workerLockupService: WorkerLockupService
 ) {
 
     @GetMapping("/me")
@@ -46,6 +50,18 @@ class WorkerController(
         return ResponseEntity.status(HttpStatus.NO_CONTENT)
             .build()
     }
+
+    @GetMapping("/{workerId}")
+    private fun findByWorkerId(@PathVariable("workerId") workerId: Long): ResponseEntity<WorkerDto.Info> =
+        ResponseEntity.ok(workerLockupService.findByWorkerId(workerId))
+
+    @GetMapping("/search")
+    private fun findByCompanyId(@RequestParam("companyId") companyId: Long): ResponseEntity<List<WorkerDto.Info>> =
+        ResponseEntity.ok(workerLockupService.findByCompanyId(companyId))
+
+    @GetMapping("/")
+    private fun findAll(): ResponseEntity<List<WorkerDto.Info>> =
+        ResponseEntity.ok(workerLockupService.findAll())
 
 
 }
