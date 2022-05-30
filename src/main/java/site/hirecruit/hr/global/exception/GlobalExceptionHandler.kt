05e5124b0149b30b9ce0f6d84552f00fcb9e3ede
siwony_ <siwon103305@gmail.com>
2022-05-30 16:15:49 +0900

    Add GlobalExceptionHandler
    Description global한 exception을 처리하는 예외 헨들러입니다.

---
+++ src/main/java/site/hirecruit/hr/global/exception/GlobalExceptionHandler.kt
@@ -0,0 +1,23 @@
+package site.hirecruit.hr.global.exception
+
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+import org.springframework.web.client.HttpClientErrorException
+import org.springframework.web.client.HttpStatusCodeException
+import site.hirecruit.hr.global.exception.model.ExceptionResponseEntity
+
+/**
+ * Global한 예외를 핸들링 하는 Handler입니다.
+ *
+ * @author 정시원
+ * @since 1.0
+ */
+@RestControllerAdvice
+class GlobalExceptionHandler {
+
+    @ExceptionHandler(HttpStatusCodeException::class)
+    private fun httpStatusException(ex: HttpClientErrorException): ResponseEntity<ExceptionResponseEntity> =
+        ResponseEntity.status(ex.statusCode.value())
+            .body(ExceptionResponseEntity.of(ex))
+}
