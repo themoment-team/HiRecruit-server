a419d89782ea27b9b50b2134c745f4824d51da64
siwony_ <siwon103305@gmail.com>
2022-06-07 22:04:33 +0900

    Update cookie prod-test profile 설정

--- src/main/java/site/hirecruit/hr/global/security/SecurityConfig.kt
+++ src/main/java/site/hirecruit/hr/global/security/SecurityConfig.kt
@@ -1,9 +1,11 @@
 package site.hirecruit.hr.global.security
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService
 import org.springframework.security.oauth2.core.user.OAuth2User
@@ -22,12 +24,19 @@ import site.hirecruit.hr.domain.auth.entity.Role
 @Profile("default", "local", "prod-test")
 class SecurityConfig(
     private val oauth2UserService: OAuth2UserService<OAuth2UserRequest, OAuth2User>,
-    ) : WebSecurityConfigurerAdapter() {
+    private val authenticationSuccessHandler: AuthenticationSuccessHandler
+) : WebSecurityConfigurerAdapter() {
 
     override fun configure(http: HttpSecurity) {
         http
             .csrf().disable()
             .headers().frameOptions().disable()
+
+        http
+            .sessionManagement{
+                it.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED) // HttpSession이 필요할 떄만 생성된다.
+                it.maximumSessions(1) // 세션은 1개만 허용한다.
+            }
 
         http
             .authorizeRequests()
@@ -57,9 +66,7 @@ class SecurityConfig(
             oauth2Login.redirectionEndpoint{
                 it.baseUri("/api/v1/auth/oauth2/redirection-endpoint")
             }
-            oauth2Login.successHandler { _, response, _ ->
-                response.sendRedirect("/api/v1/auth/oauth2/success") // oauth2 login에 성공하면 해당 uri로 redirect
-            }
+            oauth2Login.successHandler(authenticationSuccessHandler)
         }
     }
 }
