2540aa01294e412cee0a60de5e7d93caf67df619
johnjihwan <s20062@gsm.hs.kr>
2022-06-09 15:34:27 +0900

    Update wrapping timezone setter in class

--- src/main/java/site/hirecruit/hr/HRApplication.kt
+++ src/main/java/site/hirecruit/hr/HRApplication.kt
@@ -10,15 +10,16 @@ import javax.annotation.PostConstruct
 private val log = KotlinLogging.logger {}
 
 @SpringBootApplication
-class HRApplication
+class HRApplication {
 
-@PostConstruct
-fun applicationTimeZoneSetter(): Unit {
+    @PostConstruct
+    fun applicationTimeZoneSetter() {
 
-    val timeZone = TimeZone.getTimeZone("Asia/Seoul")
-    TimeZone.setDefault(timeZone)
+        val timeZone = TimeZone.getTimeZone("Asia/Seoul")
+        TimeZone.setDefault(timeZone)
 
-    log.info { "HiRecruit Backend Application TimeZone was set: ${LocalDateTime.now()}" }
+        log.info { "HiRecruit Backend Application TimeZone was set: ${LocalDateTime.now()}" }
+    }
 }
 
 fun main(args: Array<String>) {
