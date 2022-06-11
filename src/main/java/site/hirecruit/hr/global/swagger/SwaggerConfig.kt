0ce01d2378e2c984f6271c7bb50953b49d647447
siwony_ <siwon103305@gmail.com>
2022-06-11 17:58:29 +0900

    Update swagger config java to kotlin

---
+++ src/main/java/site/hirecruit/hr/global/swagger/SwaggerConfig.kt
@@ -0,0 +1,32 @@
+package site.hirecruit.hr.global.swagger
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import springfox.documentation.builders.ApiInfoBuilder
+import springfox.documentation.builders.PathSelectors
+import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.service.ApiInfo
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
+
+@Configuration
+class SwaggerConfig {
+    @Bean
+    fun api(): Docket {
+        return Docket(DocumentationType.OAS_30)
+            .apiInfo(apiInfo())
+            .useDefaultResponseMessages(false)
+            .select()
+            .apis(RequestHandlerSelectors.basePackage("site.hirecruit.hr.domain"))
+            .paths(PathSelectors.ant("/api/**"))
+            .build()
+    }
+
+    private fun apiInfo(): ApiInfo {
+        return ApiInfoBuilder()
+            .title("HiRecruit API SERVER")
+            .description("HiRecruit API DOC")
+            .version("1.0")
+            .build()
+    }
+}
