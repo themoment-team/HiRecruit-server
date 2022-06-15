f855d79968d4592e7b2adcd1e3b2c07ec6a62558
johnjihwan <s20062@gsm.hs.kr>
2022-06-15 23:38:54 +0900

    Add hr support email request

--- src/main/java/site/hirecruit/hr/thirdParty/aws/ses/service/SesFactoryServiceImpl.kt
+++ src/main/java/site/hirecruit/hr/thirdParty/aws/ses/service/SesFactoryServiceImpl.kt
@@ -1,12 +1,21 @@
-package site.hirecruit.hr.thirdParty.aws.ses.service.impl
+package site.hirecruit.hr.thirdParty.aws.ses.service
 
 import org.springframework.stereotype.Service
 import site.hirecruit.hr.thirdParty.aws.ses.dto.SesRequestDto
-import site.hirecruit.hr.thirdParty.aws.ses.service.SesFactoryService
 
+/**
+ * 개발자가 aws SES 서비스를 사용하고 싶을 때 의존해야하는 Service
+ *
+ * @author 전지환
+ * @since 1.2.0
+ */
 @Service
 class SesFactoryServiceImpl: SesFactoryService {
-    override fun sendSingleMessageToSingleDestination(sesRequestDto: SesRequestDto) {
+
+    /**
+     * 에메일 단건을 단일 사용자에게 보내는 서비스
+     */
+    override fun sendSingleMessageToSingleDestination(sesRequest: SesRequestDto) {
         TODO("Not yet implemented")
     }
 }
