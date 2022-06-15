e8fd640147d04d05b2fd15a3fb1906bf89dc223d
siwony_ <siwon103305@gmail.com>
2022-06-15 16:57:30 +0900

    Update WorkerRegistrationServiceImpl.registratino에 event를 발생하는 코드 추가

--- src/main/java/site/hirecruit/hr/domain/worker/service/WorkerRegistrationServiceImpl.kt
+++ src/main/java/site/hirecruit/hr/domain/worker/service/WorkerRegistrationServiceImpl.kt
@@ -1,6 +1,7 @@
 package site.hirecruit.hr.domain.worker.service
 
 import mu.KotlinLogging
+import org.springframework.context.ApplicationEventPublisher
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Propagation
@@ -12,6 +13,7 @@ import site.hirecruit.hr.domain.company.repository.CompanyRepository
 import site.hirecruit.hr.domain.worker.dto.WorkerDto
 import site.hirecruit.hr.domain.worker.entity.WorkerEntity
 import site.hirecruit.hr.domain.worker.repository.WorkerRepository
+import site.hirecruit.hr.global.event.WorkerRegistrationEvent
 
 val log = KotlinLogging.logger {}
 
@@ -25,11 +27,13 @@ val log = KotlinLogging.logger {}
 class WorkerRegistrationServiceImpl(
     private val userRepository: UserRepository,
     private val workerRepository: WorkerRepository,
-    private val companyRepository: CompanyRepository
+    private val companyRepository: CompanyRepository,
+    private val publisher: ApplicationEventPublisher
 ): WorkerRegistrationService {
 
     @Transactional(propagation = Propagation.REQUIRES_NEW)
     override fun registration(authUserInfo: AuthUserInfo, registrationDto: WorkerDto.Registration): WorkerDto.Info{
+        publisher.publishEvent(WorkerRegistrationEvent(authUserInfo))
         val userEntity = userRepository.findByGithubId(authUserInfo.githubId)
             ?: throw IllegalStateException("Cannot found UserEntity, githubId='${authUserInfo.githubId}'")
         val companyEntity = companyRepository.findByIdOrNull(registrationDto.companyId)
