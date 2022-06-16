7f023b610d974e3a9abbbc92aea7ef80fb613189
johnjihwan <s20062@gsm.hs.kr>
2022-06-16 23:33:32 +0900

    Add verificationEmailSender 로직을 sync 하게 처리함

--- src/main/java/site/hirecruit/hr/domain/mentor/verify/service/MentorVerificationServiceImpl.kt
+++ src/main/java/site/hirecruit/hr/domain/mentor/verify/service/MentorVerificationServiceImpl.kt
@@ -1,12 +1,19 @@
 package site.hirecruit.hr.domain.mentor.verify.service
 
+import mu.KotlinLogging
 import org.springframework.stereotype.Service
+import site.hirecruit.hr.domain.emailTemplate.verifyEmail.VerifyEmailServiceImpl
 import site.hirecruit.hr.domain.mentor.verify.MentorEmailVerificationCodeEntity
 import site.hirecruit.hr.domain.mentor.verify.repository.MentorEmailVerificationCodeRepository
 import site.hirecruit.hr.global.util.randomNumberGenerator
+import site.hirecruit.hr.thirdParty.aws.ses.dto.SesRequestDto
+
+
+private val log = KotlinLogging.logger {}
 
 @Service
 class MentorVerificationServiceImpl(
+    private val verifyEmailServiceImpl: VerifyEmailServiceImpl,
     private val mentorEmailVerificationCodeRepository: MentorEmailVerificationCodeRepository
 ) : MentorVerificationService{
 
@@ -17,13 +24,25 @@ class MentorVerificationServiceImpl(
      * @param workerId 멘토를 신청한 재직자 email
      * @return workerEmail 전송에 성공한 toAddress
      */
-    override fun sendVerificationCode(workerId: Long, workerEmail: String) : String {
+    override fun sendVerificationCode(workerId: Long, workerEmail: String, workerName: String) : String {
         // 난수 인증코드 생성
         val verificationCode = randomNumberGenerator(6)
         val mentorEmailVerificationCodeEntity = MentorEmailVerificationCodeEntity(workerId, verificationCode)
 
-        // 난수 저장 with workerId
+        // templateSesReqeustDto 작성
+        val mentorEmailVerificationEmailRequest = SesRequestDto.TemplateSesRequestDto(
+            "HiRecruitEmailAuthenticationTemplate",
+            "{ \"name\":\"${workerName}\", \"authenticationCode\":\"${verificationCode}\" }",
+            SesRequestDto.DestinationDto(null, null, listOf(workerEmail))
+        )
+
+        // 인증코드 보내기 v1.2.1 async
+        verifyEmailServiceImpl.sendEmailVerificationSES(mentorEmailVerificationEmailRequest)
+        log.info { "======== 인증번호 sendEmail success =======" }
+
+        // workerId : 인증번호 저장 시도
         mentorEmailVerificationCodeRepository.save(mentorEmailVerificationCodeEntity)
+        log.info { "=========== 인증번호 redis 저장 완료 ============" }
 
         return workerEmail
     }
