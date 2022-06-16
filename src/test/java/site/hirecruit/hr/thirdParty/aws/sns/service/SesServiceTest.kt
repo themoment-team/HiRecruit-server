e0ea86c8bef890a72faf0fa1ae16f83cf122f48c
johnjihwan <s20062@gsm.hs.kr>
2022-06-16 13:48:19 +0900

    Add 실제 전송 성공

--- src/test/java/site/hirecruit/hr/thirdParty/aws/sns/service/SesServiceTest.kt
+++ src/test/java/site/hirecruit/hr/thirdParty/aws/sns/service/SesServiceTest.kt
@@ -1,11 +1,16 @@
 package site.hirecruit.hr.thirdParty.aws.sns.service
 
+import io.mockk.verify
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import site.hirecruit.hr.domain.test_util.LocalTest
 import site.hirecruit.hr.thirdParty.aws.service.SesCredentialService
+import site.hirecruit.hr.thirdParty.aws.ses.dto.SesRequestDto
+import site.hirecruit.hr.thirdParty.aws.ses.service.HrSesServiceImpl
+import site.hirecruit.hr.thirdParty.aws.ses.service.component.templateEmail.SesTemplateEmailComponent
 import software.amazon.awssdk.services.sesv2.model.*
 
 @LocalTest
@@ -47,4 +52,26 @@ class SesServiceTest {
         assertThat(sendEmail.sdkHttpResponse().isSuccessful).isTrue
     }
 
+    @Test
+    @DisplayName("단건의 templateEmail을 destination에게 정상적으로 보낼 수 있다.")
+    fun aSesMessageSendSuccessful(
+        @Autowired sesFactoryService: HrSesServiceImpl,
+        @Autowired sesTemplateEmailComponent: SesTemplateEmailComponent,
+    ){
+
+        val templateName = "HiRecruitEmailAuthenticationTemplate"
+        val templateData = "{ \"name\":\"이선우\", \"authenticationCode\":\"1234\" }"
+        val to = "hirecruit@gsm.hs.kr"
+
+        // Given
+        val templateSesRequestDto = SesRequestDto.TemplateSesRequestDto(
+            templateName, templateData, SesRequestDto.DestinationDto(listOf(), listOf(), listOf(to))
+        )
+
+        // when
+        sesFactoryService.sendEmailWithEmailTemplate(templateSesRequestDto)
+
+        // then: 1. hr support 전용 SES Request 를 만든다.
+//        verify(exactly=1) {sesTemplateEmailComponent.createTemplateEmailRequest(templateSesRequestDto) }
+    }
 }
