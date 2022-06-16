03acbedf31692813c74f995a6e5307cb5d0b775a
johnjihwan <s20062@gsm.hs.kr>
2022-06-16 20:50:58 +0900

    Add verifyEmailService 를 verifyWorkerEmail 로직과 분리 함

--- src/main/java/site/hirecruit/hr/domain/emailTemplate/verifyEmail/VerifyEmailServiceImpl.kt
+++ src/main/java/site/hirecruit/hr/domain/emailTemplate/verifyEmail/VerifyEmailServiceImpl.kt
@@ -1,32 +1,14 @@
-package site.hirecruit.hr.domain.emailTemplate.verifyEmail.service
+package site.hirecruit.hr.domain.emailTemplate.verifyEmail
 
 import org.springframework.stereotype.Service
-import site.hirecruit.hr.global.util.randomNumberGenerator
 import site.hirecruit.hr.thirdParty.aws.ses.dto.SesRequestDto
 import site.hirecruit.hr.thirdParty.aws.ses.service.HrSesService
 
 @Service
-class VerifyEmailServiceImpl(
-    private val hrSesServiceImpl: HrSesService
-) : VerifyEmailService{
+class VerifyEmailServiceImpl(private val hrSesServiceImpl: HrSesService){
 
-    /**
-     * 6자리의 인증번호를 email로 보내는 함수이다.
-     */
-    override fun sendVerificationCode(workerId: Long, workerName: String, targetEmail: String): String? {
-        // 난수 생성
-        val randomNumber = randomNumberGenerator(6)
-
-        // 난수 저장 with workerId
-
-
-        //
-        return null
+    fun sendEmailVerificationSES(templateSesRequestDto: SesRequestDto.TemplateSesRequestDto): List<String> {
+        return hrSesServiceImpl.sendEmailWithEmailTemplate(templateSesRequestDto)
     }
-
-    override fun sendEmailVerificationSES(templateSesRequestDto: SesRequestDto.TemplateSesRequestDto): String {
-        hrSesServiceImpl.sendEmailWithEmailTemplate(templateSesRequestDto)
-    }
-
 
 }
