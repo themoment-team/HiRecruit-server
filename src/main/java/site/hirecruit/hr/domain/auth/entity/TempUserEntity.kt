a72496e7beaa3a8317f790b0864505b544636f8f
siwony_ <siwon103305@gmail.com>
2022-06-16 15:59:08 +0900

    Update TempUserEntity redis to sql

--- src/main/java/site/hirecruit/hr/domain/auth/entity/TempUserEntity.kt
+++ src/main/java/site/hirecruit/hr/domain/auth/entity/TempUserEntity.kt
@@ -1,18 +1,19 @@
 package site.hirecruit.hr.domain.auth.entity
 
-import org.springframework.data.annotation.Id
-import org.springframework.data.redis.core.RedisHash
 import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.Id
+import javax.persistence.Table
+
 /**
  * 임시 유저를 저장하는 Entity
- * Redis namespace는 temp_user를 사용하고 1시간의 유효기간을 갖습니다.
  *
  * @author 정시원
  */
-@RedisHash(value = "temp_user", timeToLive = 60 * 60)
+@Entity @Table(name = "temp_user")
 class TempUserEntity(
-    @Id
-    @Column(name = "github_id")
+
+    @Id @Column(name = "github_id")
     val githubId: Long,
 
     @Column(name = "profile_uri")
@@ -20,7 +21,7 @@ class TempUserEntity(
 ) {
 
     @javax.persistence.Transient
-    var role = Role.GUEST // 임시 유저의 권한은 GUEST이다.
+    final var role = Role.GUEST // 임시 유저의 권한은 GUEST이다.
         private set
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
