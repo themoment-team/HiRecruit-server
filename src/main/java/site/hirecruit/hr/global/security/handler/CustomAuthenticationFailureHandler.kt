a20880bad14b8ec3bfab98c2b35cad3dc1e554ef
siwony_ <siwon103305@gmail.com>
2022-06-17 10:45:40 +0900

    Add AuthenticationFailHandler
    Description 인증 실패시 동작하는 handler

---
+++ src/main/java/site/hirecruit/hr/global/security/handler/CustomAuthenticationFailureHandler.kt
@@ -0,0 +1,69 @@
+package site.hirecruit.hr.global.security.handler
+
+import mu.KotlinLogging
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.security.core.AuthenticationException
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException
+import org.springframework.security.web.authentication.AuthenticationFailureHandler
+import org.springframework.stereotype.Component
+import org.springframework.web.servlet.HandlerExceptionResolver
+import org.springframework.web.util.UriComponentsBuilder
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+private val log = KotlinLogging.logger {  }
+
+/**
+ * Security 인증 실패 핸들러
+ *
+ * @author 정시원
+ * @since 1.1.2
+ */
+@Component
+class CustomAuthenticationFailureHandler(
+    @Value("\${oauth2.login.success.redirect-base-uri}") val redirectBaseUri: String,
+    @Qualifier("handlerExceptionResolver") private val resolver: HandlerExceptionResolver
+): AuthenticationFailureHandler {
+
+    /**
+     * Authentication 실패시 예외를 핸들링 하는 메서드
+     * OAuth2 인증 예외라면 [SecurityExceptionHandler.oauth2AuthenticationException]에서 예외를 헨들링한다.
+     * 에외처리는 Hirecruit GET /api/v1/auth/oauth2/authorization/github API 명세를 확인해주세요
+     *
+     * @see SecurityExceptionHandler.oauth2AuthenticationException
+     * @param exception Authentication실패에 대한 예외
+     */
+    override fun onAuthenticationFailure(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        exception: AuthenticationException
+    ) {
+        val redirectUriBuilder = UriComponentsBuilder.fromUriString(redirectBaseUri)
+        if(exception is OAuth2AuthenticationException){ // OAuth2 로그인 예외라면
+            log.debug { "OAuth2AuthenticationException error code = '${exception.error.errorCode}'" }
+            oAuth2AuthenticationExceptionHandling(request, response, exception)
+        } else{
+            unknownExceptionHandling(response, redirectUriBuilder)
+            log.error(exception){ "Unknown Authentication Exception"}
+        }
+    }
+
+    private fun oAuth2AuthenticationExceptionHandling(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        ex: OAuth2AuthenticationException
+    ){
+        resolver.resolveException(request, response, null, ex)
+    }
+
+    private fun unknownExceptionHandling(response: HttpServletResponse, redirectUrlBuilder: UriComponentsBuilder){
+        response.sendRedirect(
+            redirectUrlBuilder
+                .queryParam("login", "fail")
+                .queryParam("server_error", true)
+                .build().toUriString()
+        )
+    }
+
+}
