6a03fd0103dad2651ae60e01aa1ebbb3bdd1283f
siwony_ <siwon103305@gmail.com>
2022-06-17 19:32:59 +0900

    Update Bean validator의 class level의 검증 메시지 전달 로직

--- src/main/java/site/hirecruit/hr/global/exception/GlobalExceptionHandler.kt
+++ src/main/java/site/hirecruit/hr/global/exception/GlobalExceptionHandler.kt
@@ -3,6 +3,7 @@ package site.hirecruit.hr.global.exception
 import mu.KotlinLogging
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.validation.BeanPropertyBindingResult
 import org.springframework.validation.BindingResult
 import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
@@ -38,15 +39,30 @@ class GlobalExceptionHandler {
      * 반환 메세지 예시
      * ```json
      * {
-     *    "message": "'name':'공백일 수 없습니다.', 'homepageUri':'올바른 URL이어야 합니다.', 'location':'공백일 수 없습니다.', 'companyImgUri':'공백일 수 없습니다.'"
+     *    "message": "'name':'공백일 수 없습니다.', 'homepageUri':'올바른 URL이어야 합니다.', "해당 company(회사)는 이미 존재합니다."
      * }
      * ```
      */
     @ExceptionHandler(MethodArgumentNotValidException::class)
-    private fun validationException(ex: MethodArgumentNotValidException): ResponseEntity<ExceptionResponseEntity>{
+    private fun validationException(ex: MethodArgumentNotValidException): ResponseEntity<ExceptionResponseEntity> {
+        val statusCode = HttpStatus.BAD_REQUEST.value()
+
         val bindingResult: BindingResult = ex.bindingResult
+        log.debug { "BindResult = '$bindingResult'" }
 
         val errorResultBuilder = StringBuilder()
+
+        /**
+         * objectError메시지 전달
+         */
+        for (objectError in bindingResult.globalErrors){
+            val errorMessage = objectError.defaultMessage
+
+            errorResultBuilder.append("'${errorMessage}.'")
+            errorResultBuilder.append(", ")
+        }
+
+
         for (fieldError in bindingResult.fieldErrors) {
             /**
              * 유효성검사에 통과하지 못한 field name, field name의 접두사에 "_"가 있다면 제거한다.
@@ -67,7 +83,7 @@ class GlobalExceptionHandler {
         }
 
         val errorResult = errorResultBuilder.removeSuffix(", ").toString() // 마지막 리스트일 경우  ", " 문자열을 제거한다.
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST.value())
+        return ResponseEntity.status(statusCode)
             .body(ExceptionResponseEntity(errorResult))
     }
 }
