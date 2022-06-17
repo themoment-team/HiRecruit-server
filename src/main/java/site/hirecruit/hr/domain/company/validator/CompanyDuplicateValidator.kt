b45ef0d4fadf004fb80c3ed3d34e368690e90706
siwony_ <siwon103305@gmail.com>
2022-06-17 14:48:39 +0900

    Add CompanyNotDuplicate annotation, CompanyDuplicateValidator

---
+++ src/main/java/site/hirecruit/hr/domain/company/validator/CompanyDuplicateValidator.kt
@@ -0,0 +1,26 @@
+package site.hirecruit.hr.domain.company.validator
+
+import org.springframework.stereotype.Component
+import site.hirecruit.hr.domain.company.dto.CompanyDto
+import site.hirecruit.hr.domain.company.repository.CompanyRepository
+import site.hirecruit.hr.domain.company.validator.annoation.CompanyNotDuplicate
+import javax.validation.ConstraintValidator
+import javax.validation.ConstraintValidatorContext
+
+/**
+ * [CompanyNotDuplicate] annotation에 대한 validator
+ *
+ * @author 정시원
+ * @since 1.1.2
+ */
+@Component
+class CompanyDuplicateValidator(
+        private val companyRepository: CompanyRepository
+): ConstraintValidator<CompanyNotDuplicate, CompanyDto.Create>
+ {
+     override fun isValid(value: CompanyDto.Create?, context: ConstraintValidatorContext?): Boolean {
+         TODO("Not yet implemented")
+     }
+
+
+ }
---
+++ src/main/java/site/hirecruit/hr/domain/company/validator/annoation/CompanyNotDuplicate.kt
@@ -0,0 +1,25 @@
+package site.hirecruit.hr.domain.company.validator.annoation
+
+import site.hirecruit.hr.domain.company.validator.CompanyDuplicateValidator
+import site.hirecruit.hr.domain.company.validator.CompanyExistValidator
+import javax.validation.Constraint
+import javax.validation.Payload
+import kotlin.reflect.KClass
+
+/**
+ * Company가 중복으로 존재하는지 검증하는 validator
+ * 해당 annotation은 CompanyDto.Create에서만 사용할 수 있습니다.
+ *
+ * @see CompanyExistValidator
+ * @author 정시원
+ * @since 1.1.2
+ */
+@MustBeDocumented
+@Constraint(validatedBy = [CompanyDuplicateValidator::class])
+@Target(AnnotationTarget.TYPE)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class CompanyNotDuplicate(
+    val message: String = "해당 company(회사)는 이미 존재합니다.",
+    val groups: Array<KClass<*>> = [],
+    val payload: Array<KClass<out Payload>> = [],
+)
