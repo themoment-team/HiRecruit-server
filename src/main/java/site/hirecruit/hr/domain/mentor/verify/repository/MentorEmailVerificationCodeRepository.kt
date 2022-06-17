ecfc1a7ade5bf879e8ddb75c8163f8f667557ef2
johnjihwan <s20062@gsm.hs.kr>
2022-06-17 12:14:29 +0900

    Update entity 네이밍

--- src/main/java/site/hirecruit/hr/domain/mentor/verify/repository/MentorEmailVerificationCodeRepository.kt
+++ src/main/java/site/hirecruit/hr/domain/mentor/verify/repository/MentorEmailVerificationCodeRepository.kt
@@ -2,7 +2,7 @@ package site.hirecruit.hr.domain.mentor.verify.repository
 
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
-import site.hirecruit.hr.domain.mentor.verify.MentorEmailVerificationCodeEntity
+import site.hirecruit.hr.domain.mentor.verify.entity.MentorEmailVerificationCodeEntity
 
 @Repository
 interface MentorEmailVerificationCodeRepository : CrudRepository<MentorEmailVerificationCodeEntity, Long>
