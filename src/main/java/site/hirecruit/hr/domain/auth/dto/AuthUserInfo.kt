98ee744ef691f0e27389c9a2bea35f4f9c65fef0
siwony_ <siwon103305@gmail.com>
2022-06-21 20:14:08 +0900

    Update AuthUserInfo to data class

--- src/main/java/site/hirecruit/hr/domain/auth/dto/AuthUserInfo.kt
+++ src/main/java/site/hirecruit/hr/domain/auth/dto/AuthUserInfo.kt
@@ -17,38 +17,11 @@ import site.hirecruit.hr.domain.auth.entity.Role
  */
 @Component
 @Scope(value = WebApplicationContext.SCOPE_SESSION, proxyMode = ScopedProxyMode.TARGET_CLASS)
-open class AuthUserInfo @QueryProjection constructor(
+data class AuthUserInfo @QueryProjection constructor(
     val githubId: Long,
     val githubLoginId: String,
     val name: String,
     val email: String?,
     val profileImgUri: String,
     val role: Role
-): java.io.Serializable {
-    override fun toString(): String {
-        return "AuthUserInfo(githubId=$githubId, name='$name', email=$email, profileUri='$profileImgUri', role=$role)"
-    }
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (other !is AuthUserInfo) return false
-
-        if (githubId != other.githubId) return false
-        if (name != other.name) return false
-        if (email != other.email) return false
-        if (profileImgUri != other.profileImgUri) return false
-        if (role != other.role) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = githubId.hashCode()
-        result = 31 * result + name.hashCode()
-        result = 31 * result + (email?.hashCode() ?: 0)
-        result = 31 * result + profileImgUri.hashCode()
-        result = 31 * result + role.hashCode()
-        return result
-    }
-
-}
+): java.io.Serializable
