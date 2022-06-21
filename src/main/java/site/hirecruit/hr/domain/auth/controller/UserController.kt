9d4b422e83ed0c2c37c094860862ca308cd65984
siwony_ <siwon103305@gmail.com>
2022-06-21 10:07:53 +0900

    Add UserController

---
+++ src/main/java/site/hirecruit/hr/domain/auth/controller/UserController.kt
@@ -0,0 +1,36 @@
+package site.hirecruit.hr.domain.auth.controller
+
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PatchMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
+import site.hirecruit.hr.domain.auth.dto.AuthUserInfo
+import site.hirecruit.hr.domain.auth.dto.UserUpdateDto
+import site.hirecruit.hr.domain.auth.service.UserUpdateService
+import site.hirecruit.hr.global.annotation.CurrentAuthUserInfo
+import springfox.documentation.annotations.ApiIgnore
+
+@RestController
+@RequestMapping("/api/v1/user")
+class UserController(
+    private val userUpdateService: UserUpdateService
+) {
+
+    @PatchMapping
+    fun updateUser(
+        @ApiIgnore
+        @CurrentAuthUserInfo
+        authUserInfo: AuthUserInfo,
+
+        @RequestBody
+        updateDto: UserUpdateDto
+    ): ResponseEntity<Unit>{
+        userUpdateService.update(updateDto, authUserInfo)
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT)
+            .build()
+    }
+}
