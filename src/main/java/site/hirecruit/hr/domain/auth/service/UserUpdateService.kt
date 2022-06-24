eb18314b883c81262f9c93802b704e320f5b2c5c
siwony_ <siwon103305@gmail.com>
2022-06-24 10:21:25 +0900

    Update UserUpdateServiceImpl.update 매서드는 앞으로 변경된 유저의 정보를 반환함

--- src/main/java/site/hirecruit/hr/domain/auth/service/UserUpdateService.kt
+++ src/main/java/site/hirecruit/hr/domain/auth/service/UserUpdateService.kt
@@ -5,8 +5,15 @@ import site.hirecruit.hr.domain.auth.dto.UserUpdateDto
 
 /**
  * User의 기본정보를 Update하는 Service
+ * @author 정시ㅜ언
+ * @since 1.2
  */
 interface UserUpdateService {
 
-    fun update(updateDto: UserUpdateDto, authUserInfo: AuthUserInfo)
+    /**
+     * 사용자의 기본 정보를 수정한다.
+     *
+     * @return 수정된 사용자 정보 DTO
+     */
+    fun update(updateDto: UserUpdateDto, authUserInfo: AuthUserInfo): AuthUserInfo
 }
