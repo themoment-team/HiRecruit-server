cf3055c0a4f716e4d06ec7add74cf812dcebe40a
siwony_ <siwon103305@gmail.com>
2022-06-25 18:02:04 +0900

    Update user update시 mentor 가 email를 수정하면 WORKER로 강등

--- src/main/java/site/hirecruit/hr/domain/auth/service/impl/UserUpdateServiceImpl.kt
+++ src/main/java/site/hirecruit/hr/domain/auth/service/impl/UserUpdateServiceImpl.kt
@@ -1,8 +1,11 @@
 package site.hirecruit.hr.domain.auth.service.impl
 
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 import site.hirecruit.hr.domain.auth.dto.AuthUserInfo
 import site.hirecruit.hr.domain.auth.dto.UserUpdateDto
+import site.hirecruit.hr.domain.auth.entity.Role
+import site.hirecruit.hr.domain.auth.entity.UserEntity
 import site.hirecruit.hr.domain.auth.repository.UserRepository
 import site.hirecruit.hr.domain.auth.service.UserUpdateService
 
@@ -11,8 +14,9 @@ class UserUpdateServiceImpl(
     private val userRepository: UserRepository
 ): UserUpdateService {
 
-    override fun update(updateDto: UserUpdateDto, authUserInfo: AuthUserInfo): AuthUserInfo {
-        val userEntity = userRepository.findByGithubId(authUserInfo.githubId)
+    @Transactional
+    override fun update(updateDto: UserUpdateDto, authUserInfoBeforeUpdate: AuthUserInfo): AuthUserInfo {
+        val userEntity = userRepository.findByGithubId(authUserInfoBeforeUpdate.githubId)
             ?: throw IllegalArgumentException("Cannot found user info")
 
         updateDto.updateColumns.forEach {
@@ -23,6 +27,9 @@ class UserUpdateServiceImpl(
         }
 
         val updatedUserEntity = userRepository.save(userEntity)
+
+        if(isMentorEmailChanged(authUserInfoBeforeUpdate, updatedUserEntity))
+            updatedUserEntity.role = Role.WORKER
         return AuthUserInfo(
             githubId = updatedUserEntity.githubId,
             githubLoginId = updatedUserEntity.githubLoginId,
@@ -31,6 +38,13 @@ class UserUpdateServiceImpl(
             profileImgUri = updatedUserEntity.profileImgUri,
             role = updatedUserEntity.role
         )
+    }
+
+    private fun isMentorEmailChanged(authUserInfoBeforeUpdate: AuthUserInfo, updatedUserEntity: UserEntity): Boolean {
+        if(authUserInfoBeforeUpdate.role != Role.MENTOR) // 멘토가 아니라면
+            return false
+
+        return authUserInfoBeforeUpdate.email.equals(updatedUserEntity.email).not()
     }
 
 
