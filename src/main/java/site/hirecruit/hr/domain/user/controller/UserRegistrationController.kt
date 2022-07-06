ac9ced7e5da0f0bad4f5228d99fae3675d97c3c8
siwony_ <siwon103305@gmail.com>
2022-07-06 09:27:47 +0900

    Add UserRegistrationController and AuthController Deprecated

--- src/main/java/site/hirecruit/hr/domain/user/controller/UserRegistrationController.kt
+++ src/main/java/site/hirecruit/hr/domain/user/controller/UserRegistrationController.kt
@@ -1,22 +1,25 @@
-package site.hirecruit.hr.domain.auth.controller
+package site.hirecruit.hr.domain.user.controller
 
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.*
-import site.hirecruit.hr.domain.user.service.UserRegistrationService
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 import site.hirecruit.hr.domain.auth.dto.AuthUserInfo
+import site.hirecruit.hr.domain.auth.service.TempUserRegistrationService
 import site.hirecruit.hr.domain.user.dto.UserRegistrationDto
+import site.hirecruit.hr.domain.user.service.UserRegistrationService
 import site.hirecruit.hr.global.annotation.CurrentAuthUserInfo
 import site.hirecruit.hr.global.util.CookieUtil
 import springfox.documentation.annotations.ApiIgnore
 import javax.servlet.http.HttpServletResponse
 import javax.validation.Valid
 
-@Deprecated("해당 controller는 추후 사용되지 않을 예정입니다. domain.user.controller.UserRegistrationController로 해당 controller의 역할이 이전될 예정입니다.")
 @RestController
-@RequestMapping("/api/v1/auth")
-class AuthController(
+@RequestMapping("/api/v1/user")
+class UserRegistrationController(
     private val userRegistrationService: UserRegistrationService,
     @Value("\${hr.domain}") private val hrDomain: String
 ) {
@@ -26,16 +29,15 @@ class AuthController(
         @CurrentAuthUserInfo @ApiIgnore
         authUserInfo: AuthUserInfo,
 
-        @RequestBody  @Valid
+        @RequestBody @Valid
         userRegistrationDto: UserRegistrationDto,
 
         response: HttpServletResponse
-    ): ResponseEntity<AuthUserInfo>{
+    ): ResponseEntity<AuthUserInfo> {
         val registeredAuthUserInfo = userRegistrationService.registration(authUserInfo, userRegistrationDto)
 
         response.addCookie(CookieUtil.userTypeCookie(registeredAuthUserInfo.role.name, hrDomain)) // 등록된 유저의 role을 USER_TYPE 쿠키로 넘겨줌
         return ResponseEntity.status(HttpStatus.CREATED)
             .body(registeredAuthUserInfo)
     }
-
 }
