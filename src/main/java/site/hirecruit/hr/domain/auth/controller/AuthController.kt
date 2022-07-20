4a8a4581d66f33dbbbf90612a485b7cb60290eb6
siwony_ <siwon103305@gmail.com>
2022-07-20 20:12:12 +0900

    UPdate AUthController

--- src/main/java/site/hirecruit/hr/domain/auth/controller/AuthController.kt
+++ src/main/java/site/hirecruit/hr/domain/auth/controller/AuthController.kt
@@ -1,12 +1,15 @@
 package site.hirecruit.hr.domain.auth.controller
 
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
-import site.hirecruit.hr.domain.user.service.legacy.UserRegistrationService
 import site.hirecruit.hr.domain.auth.dto.AuthUserInfo
+import site.hirecruit.hr.domain.user.dto.RegularUserRegistrationDto
+import site.hirecruit.hr.domain.user.dto.RegularUserRegistrationRequestDto
 import site.hirecruit.hr.domain.user.dto.UserRegistrationDto
+import site.hirecruit.hr.domain.user.service.UserRegistrationService
 import site.hirecruit.hr.global.annotation.CurrentAuthUserInfo
 import site.hirecruit.hr.global.util.CookieUtil
 import springfox.documentation.annotations.ApiIgnore
@@ -17,7 +20,9 @@ import javax.validation.Valid
 @RestController
 @RequestMapping("/api/v1/auth")
 class AuthController(
-    private val userRegistrationService: UserRegistrationService,
+    @Qualifier("workerUserRegistrationService")
+    private val regularUserRegistrationService: UserRegistrationService<RegularUserRegistrationDto>,
+
     @Value("\${hr.domain}") private val hrDomain: String
 ) {
 
@@ -26,12 +31,18 @@ class AuthController(
         @CurrentAuthUserInfo @ApiIgnore
         authUserInfo: AuthUserInfo,
 
-        @RequestBody  @Valid
-        userRegistrationDto: UserRegistrationDto,
+        @RequestBody @Valid
+        userRegistrationDto: RegularUserRegistrationRequestDto,
 
         response: HttpServletResponse
-    ): ResponseEntity<AuthUserInfo>{
-        val registeredAuthUserInfo = userRegistrationService.registration(authUserInfo, userRegistrationDto)
+    ): ResponseEntity<AuthUserInfo> {
+        val regularUserRegistrationDto = RegularUserRegistrationDto(
+            githubId = authUserInfo.githubId,
+            githubLoginId = authUserInfo.githubLoginId,
+            profileImgUri = authUserInfo.profileImgUri,
+            userRegistrationInfo = userRegistrationDto
+        )
+        val registeredAuthUserInfo = regularUserRegistrationService.registration(regularUserRegistrationDto)
 
         response.addCookie(CookieUtil.userTypeCookie(registeredAuthUserInfo.role.name, hrDomain)) // 등록된 유저의 role을 USER_TYPE 쿠키로 넘겨줌
         return ResponseEntity.status(HttpStatus.CREATED)
